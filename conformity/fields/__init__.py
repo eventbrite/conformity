ede4a1624694cf4d366063ed6732a08182ad6b92
Bartek Ogryczak <bartek@eventbrite.com>
2016-10-19 11:40:14 -0700

    renaming datetime to temporal + tests

--- conformity/fields/__init__.py
+++ conformity/fields/__init__.py
@@ -1,4 +1,4 @@
 from .basic import Base, Constant, Anything, Boolean, Integer, Float, ByteString, UnicodeString
 from .collections import List, Dictionary
 from .meta import Polymorph
-from .datetime import DateTime, Date, TimeDelta, Time, TZInfo
+from .temporal import DateTime, Date, TimeDelta, Time, TZInfo
--- conformity/fields/temporal.py
+++ conformity/fields/temporal.py
@@ -4,7 +4,7 @@ import datetime
 from .basic import Base
 
 
-class DatetimeBase(Base):
+class TemporalBase(Base):
     def __init__(self, gt=None, lt=None, gte=None, lte=None):
         self.gt = gt
         self.lt = lt
@@ -12,38 +12,39 @@ class DatetimeBase(Base):
         self.lte = lte
 
     def errors(self, value):
-        if not isinstance(value, self.valid_type):
+        if not type(value) == self.valid_type:
+            # using stricter type checking, because date is subclass of datetime, but they're not comparable
             return ["Not a %s instance" % self.valid_noun]
         elif self.gt is not None and value <= self.gt:
             return ["Value not > %s" % self.gt]
-        elif self.lt is not None and value >= self.ly:
+        elif self.lt is not None and value >= self.lt:
             return ["Value not < %s" % self.lt]
         elif self.gte is not None and value < self.gte:
             return ["Value not >= %s" % self.gte]
-        elif self.lte is not None and value > self.lye:
+        elif self.lte is not None and value > self.lte:
             return ["Value not <= %s" % self.lte]
 
 
-class DateTime(DatetimeBase):
-    valid_type = (datetime.date, datetime.datetime)
+class DateTime(TemporalBase):
+    valid_type = datetime.datetime
     valid_noun = "datetime.datetime"
 
 
-class Date(DatetimeBase):
+class Date(TemporalBase):
     valid_type = datetime.date
     valid_noun = "datetime.date"
 
 
-class TimeDelta(DatetimeBase):
+class TimeDelta(TemporalBase):
     valid_type = datetime.timedelta
     valid_noun = "datetime.timedelta"
 
 
-class Time(DatetimeBase):
+class Time(TemporalBase):
     valid_type = datetime.time
     valid_noun = "datetime.time"
 
 
-class TZInfo(DatetimeBase):
+class TZInfo(TemporalBase):
     valid_type = datetime.tzinfo
     valid_noun = "datetime.tzinfo"
