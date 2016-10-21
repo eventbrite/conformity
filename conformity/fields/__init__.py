fb319661370ce8c822631ec06b07e3d1b57942c9
Bartek Ogryczak <bartek@eventbrite.com>
2016-10-21 13:30:51 -0700

    renaming 'collections' to 'structures' to avoid name clash with Python's built-in

--- conformity/fields/__init__.py
+++ conformity/fields/__init__.py
@@ -1,4 +1,4 @@
 from .basic import Base, Constant, Anything, Hashable, Boolean, Integer, Float, ByteString, UnicodeString
-from .collections import List, Dictionary, SchemalessDictionary
+from .structures import List, Dictionary, SchemalessDictionary
 from .meta import Polymorph
 from .temporal import DateTime, Date, TimeDelta, Time, TZInfo
