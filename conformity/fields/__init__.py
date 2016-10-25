8064e34393991a77d280478b66205eb7df59b0fe
Bartek Ogryczak <bartek@eventbrite.com>
2016-10-24 18:26:37 -0700

    a meta ObjectInstance to validate that field is of particular class or subclass

--- conformity/fields/__init__.py
+++ conformity/fields/__init__.py
@@ -1,4 +1,4 @@
 from .basic import Base, Constant, Anything, Hashable, Boolean, Integer, Float, ByteString, UnicodeString
 from .structures import List, Dictionary, SchemalessDictionary
-from .meta import Polymorph
+from .meta import Polymorph, ObjectInstance
 from .temporal import DateTime, Date, TimeDelta, Time, TZInfo
