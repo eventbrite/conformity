1654b89dc976a76df57dd5fc837dfa8a2b48820d
Andrew Godwin <andrew@aeracode.org>
2017-05-01 13:38:39 -0700

    Add Any combinatorial field (#12)

--- conformity/fields/meta.py
+++ conformity/fields/meta.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import attr
 
 from .basic import Base
+from .structures import _update_error_pointer
 from ..error import Error
 from ..utils import strip_none
 
@@ -71,4 +72,43 @@ class ObjectInstance(Base):
             # Unfortunately, this is the one sort of thing we can't represent
             # super well. Maybe add some dotted path stuff in here.
             "valid_type": repr(self.valid_type),
+        })
+
+
+class Any(Base):
+    """
+    Accepts any one of the types passed as positional arguments.
+    Intended to be used for constants but could be used with others.
+    """
+
+    description = None
+
+    def __init__(self, *args, **kwargs):
+        self.options = args
+        # We can't put a keyword argument after *args in Python 2, so we need this
+        if "description" in kwargs:
+            self.description = kwargs["description"]
+            del kwargs["description"]
+        if kwargs:
+            raise TypeError("Unknown keyword arguments: %s" % ", ".join(kwargs.keys()))
+
+    def errors(self, value):
+        result = []
+        for i, option in enumerate(self.options):
+            sub_errors = option.errors(value)
+            # If there's no errors from a sub-field, then it's all OK!
+            if not sub_errors:
+                return []
+            # Otherwise, add the errors to the overall results
+            result.extend(
+                _update_error_pointer(error, i)
+                for error in sub_errors
+            )
+        return result
+
+    def introspect(self):
+        return strip_none({
+            "type": "any",
+            "description": self.description,
+            "options": [option.introspect() for option in self.options],
         })
