253dae4f779af4f0dbdeeb0b506a02146f89a936
Nick Williams <nwilliams@eventbrite.com>
2018-02-05 16:04:43 -0600

    Released Conformity version 1.7.4
    Changelog Details:
    - Fix Travis deploy step

--- conformity/version.py
+++ conformity/version.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
 
 
-__version_info__ = (1, 7, 3)
+__version_info__ = (1, 7, 4)
 __version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
