7929f01fd03a30cdf3f3f4d239124335679010bd
Riley Mathews <rmathews@eventbrite.com>
2023-01-10 09:25:09 -0600

    lower version of coverage for python 2

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ tests_require = [
     'mypy~=0.740;python_version>"3.4"',
     'pytest>4.2,<5.4',
     'pytest-cov==2.1.0',
+    'coverage~=4.5',
     'pytest-runner',
     'pytz',
 ] + currency_requires + country_requires + spinx_requires
